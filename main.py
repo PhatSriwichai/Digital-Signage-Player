5e68936cb1ee97a405d3a571800e0ff4970ee203
PhatSriwichai <phattharaphon.sriwichai@gmail.com>
2016-09-27 22:37:30 +0700

    display ticker

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os, subprocess, sys
 import socket
-import time
+import glob, json, time, codecs
 from socketIO_client import SocketIO, BaseNamespace
 
 server_ip = '192.168.137.2'
@@ -14,6 +14,18 @@ class Namespace(BaseNamespace):
 
 def receive_action(*args):
     #print args[0]
+    try:
+        json_data = open('/home/pi/media/control.json').read()
+        control = json.loads(json_data)
+    except:
+        print "Main No Control"
+
+    try:
+        print control['control']['playlist']
+        ticker = subprocess.Popen(["chromium-browser", "-kiosk",
+                                   "/home/pi/media/ticker/"+control['control']['playlist']+"_ticker.html"])
+    except:
+        print "Main No ticker"
     if(args[0] == 'play'):
         subprocess.Popen.kill(play)
         subprocess.call(["killall", "omxplayer"])
@@ -24,17 +36,25 @@ def receive_action(*args):
         time.sleep(1)
     #elif(args[0] == 'play'):
         
-
+try:
+    json_data = open('/home/pi/media/control.json').read()
+    control = json.loads(json_data)
+    print "read success"
+except:
+    print "Main No Control"
 
 socketIO = SocketIO(server_ip, 8080, Namespace)
-play = subprocess.Popen(["python", "testPlayLoop.py"], shell=False)
+
 #time.sleep(5)
 #subprocess.Popen.kill(play)
 #subprocess.call(["killall", "/usr/bin/omxplayer.bin"])
 
 receive = subprocess.Popen(["python", "receive.py"])
 on_off = subprocess.Popen(["python", "on_off.py"])
-
+ticker = subprocess.Popen(["chromium-browser", "-kiosk",
+                                   "/home/pi/media/ticker/"+control['control']['playlist']+"_ticker.html"])
+time.sleep(5)
+play = subprocess.Popen(["python", "testPlayLoop.py"], shell=False)
 socketIO.on('action', receive_action)
 socketIO.wait()
 #while(1):
--- testPlayLoop.py
+++ testPlayLoop.py
@@ -21,26 +21,48 @@ while(1):
         print "no Media"
             
     listFile = []
-    for i in range(0,len(playlist['assets'])):
-        listFile.insert(0, playlist['assets'][i]['fileName'])
-        print 'playing' + playlist['assets'][i]['fileName']
+    if(playlist['ticker']['behavior'] == 'none'):
+        for i in range(0,len(playlist['assets'])):
+            listFile.insert(0, playlist['assets'][i]['fileName'])
+            print 'playing' + playlist['assets'][i]['fileName']
 
-        if(playlist['assets'][i]['type'] == 'video'):
-            if(playlist['assets'][i]['format'] == 'file'):
-                a = subprocess.call( [ "omxplayer", "-o", "both", "-b", '/home/pi/media/'+playlist['assets'][i]['fileName']])
-            else:
-                a = subprocess.call( [ "omxplayer", "-o", "both", "-b", playlist['assets'][i]['fileName']])
-        else:
-            with codecs.open('/home/pi/media/imageShow.txt', 'w', 'utf-8') as f:
-                f.write(playlist['assets'][i]['fileName'])
-                
-            if(playlist['assets'][i]['format'] == 'file'):
-                a = subprocess.Popen(["python", 'testImageLoop.py'])
-            else:
-                a = subprocess.Popen(["python", 'testImageLoop.py'])
-            time.sleep(20)
-            subprocess.Popen.kill(a)
-            subprocess.call(["killall", "feh"])
+            if(playlist['assets'][i]['type'] == 'video'):
+                if(playlist['assets'][i]['format'] == 'file'):
+                    a = subprocess.call( [ "omxplayer", "-o", "both", "-b", '/home/pi/media/'+playlist['assets'][i]['fileName']])
+                else:
+                    a = subprocess.call( [ "omxplayer", "-o", "both", "-b", playlist['assets'][i]['fileName']])
+            elif(playlist['assets'][i]['type'] == 'image'):
+                with codecs.open('/home/pi/media/imageShow.txt', 'w', 'utf-8') as f:
+                    f.write(playlist['assets'][i]['fileName'])
+                    
+                if(playlist['assets'][i]['format'] == 'file'):
+                    a = subprocess.Popen(["python", 'testImageLoop.py'])
+                else:
+                    a = subprocess.Popen(["python", 'testImageLoop.py'])
+                time.sleep(20)
+                subprocess.Popen.kill(a)
+                subprocess.call(["killall", "feh"])
+    else:
+        for i in range(0,len(playlist['assets'])):
+            listFile.insert(0, playlist['assets'][i]['fileName'])
+            print 'playing' + playlist['assets'][i]['fileName']
+
+            if(playlist['assets'][i]['type'] == 'video'):
+                if(playlist['assets'][i]['format'] == 'file'):
+                    a = subprocess.call( [ "omxplayer", "--win", "0 0 1024 710", "-o", "hdmi", '/home/pi/media/'+playlist['assets'][i]['fileName']])
+                else:
+                    a = subprocess.call( [ "omxplayer", "--win", "0 0 1024 710", "-o", "hdmi", playlist['assets'][i]['fileName']])
+            elif(playlist['assets'][i]['type'] == 'image'):
+                with codecs.open('/home/pi/media/imageShow.txt', 'w', 'utf-8') as f:
+                    f.write(playlist['assets'][i]['fileName'])
+                    
+                if(playlist['assets'][i]['format'] == 'file'):
+                    a = subprocess.Popen(["python", 'testImageLoop.py'])
+                else:
+                    a = subprocess.Popen(["python", 'testImageLoop.py'])
+                time.sleep(20)
+                subprocess.Popen.kill(a)
+                subprocess.call(["killall", "feh"])
             
     #print listFile
             
