bf106c4791ac58cb747b619d7167fff3a7c99b20
PhatSriwichai <phattharaphon.sriwichai@gmail.com>
2016-10-09 17:28:09 +0700

    layout complete

--- real/leftSlide.py
+++ real/leftSlide.py
@@ -33,6 +33,22 @@ elif(playlist['layout'] == '2a'):
         position = "%dx%s+%d+%d" % (int(width)-int(float(width)*0.75), height, int(float(width)*0.75), 0)
     else:
         position = "%dx%d+%d+%d" % (int(width)-int(float(width)*0.75), int(float(height)*0.9), int(float(width)*0.75), 0)
-        
+elif(playlist['layout'] == '2b'):
+    if(playlist['ticker']['behavior'] == 'none'):
+        #position = str(width-int(float(width)*0.75))+'x'+str(height)+'+'+str(int(float(width)*0.75))+'+0'
+        position = "%dx%s+%d+%d" % (int(width)-int(float(width)*0.75), height, 0, 0)
+    else:
+        position = "%dx%d+%d+%d" % (int(width)-int(float(width)*0.75), int(float(height)*0.9), 0, 0)
+elif(playlist['layout'] == '3a'):
+    if(playlist['ticker']['behavior'] == 'none'):
+        #position = str(width-int(float(width)*0.75))+'x'+str(height)+'+'+str(int(float(width)*0.75))+'+0'
+        position = "%dx%s+%d+%d" % (int(width), int(float(height)*0.2), 0, 0)
+    else:
+        position = "%dx%s+%d+%d" % (int(width), int(float(height)*0.2), 0, 0)
+elif(playlist['layout'] == '3b'):
+    if(playlist['ticker']['behavior'] == 'none'):
+        position = "%dx%s+%d+%d" % (int(width), int(float(height)*0.2), 0, int(float(height)*0.8))
+    else:
+        position = "%dx%d+%d+%d" % (int(width), int(float(height)*0.2), 0, int(float(height)*0.7))  
 fileName = open('/home/pi/media/slideShow.txt').read()
 a = subprocess.call( [ "feh", "-B", "black", "-x", "-g", position, '/home/pi/media/'+fileName])
