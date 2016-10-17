ed604b27ad5cc3830b05119473b12862fe84a4d4
Phattharaphon Sriwichai <wai.yyyyy20@gmail.com>
2016-10-17 21:38:44 +0700

    img index.html

--- real/genIndex.py
+++ real/genIndex.py
@@ -12,9 +12,9 @@ ip = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
 stringHTML = "<!DOCTYPE html>"
 stringHTML += "<html style=\"height:100%\">"
 stringHTML += "<head></head>"
-stringHTML += "<body style=\"height:100%; overflow:hidden; background-color:#1B84FB;\">"
-stringHTML += "<center><font size=\"6\" color=\"white\"><h1>SUT Signage</h1></font></center>"
+stringHTML += "<body style=\"height:100%; overflow:hidden; background-color:white;\">"
 stringHTML += "<section style=\"height:15%\"></section>"
+stringHTML += "<center><img src=\"/image/logo.png\"></center>"
 stringHTML += "<center><h1>IP Address : "+ip+"</h1></center>"
 stringHTML += "<center><h1>MAC Address : "+mac+"</h1></center></body></html>"
 with codecs.open('/home/pi/Digital-Signage-Player/real/web/index.html', 'w', encoding='utf-8') as f:
