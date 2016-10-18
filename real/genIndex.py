074b98d178b8648cf6aa8ef58cad70330143d666
Phattharaphon Sriwichai <wai.yyyyy20@gmail.com>
2016-10-19 02:15:41 +0700

    update

--- real/genIndex.py
+++ real/genIndex.py
@@ -7,14 +7,16 @@ def getHwAddr(ifname):
     info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
     return ':'.join(['%02x' % ord(char) for char in info[18:24]])
 mac = getHwAddr('eth0')
-ip = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
-
+try:
+    ip = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
+except:
+    ip = "not connecting network"
 stringHTML = "<!DOCTYPE html>"
 stringHTML += "<html style=\"height:100%\">"
 stringHTML += "<head></head>"
 stringHTML += "<body style=\"height:100%; overflow:hidden; background-color:white;\">"
 stringHTML += "<section style=\"height:15%\"></section>"
-stringHTML += "<center><img src=\"/image/logo.png\"></center>"
+stringHTML += "<center><img src=\"image/logo.png\"></center>"
 stringHTML += "<center><h1>IP Address : "+ip+"</h1></center>"
 stringHTML += "<center><h1>MAC Address : "+mac+"</h1></center></body></html>"
 with codecs.open('/home/pi/Digital-Signage-Player/real/web/index.html', 'w', encoding='utf-8') as f:
--- real/start.sh
+++ real/start.sh
@@ -4,3 +4,4 @@ cd /
 cd home/pi/Digital-Signage-Player/real
 python main.py
 cd /
+unclutter -display :0 -noevents -grab
